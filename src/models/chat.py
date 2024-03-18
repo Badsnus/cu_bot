e614219bf5cd6add91d136faab94378f7d385e39
Badsnus <anton.098.bal@gmail.com>
2024-03-18 16:31:02 +0300

    chat_name in chat model

--- src/models/chat.py
+++ src/models/chat.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    BigInteger, Column, Integer, ForeignKey, Table,
+    BigInteger, Column, Integer, ForeignKey, String, Table,
 )
 
 from src.models import Base
@@ -10,6 +10,8 @@ class Chat(Base):
 
     id = Column(Integer(), primary_key=True)
     telegram_id = Column(BigInteger(), unique=True, index=True)
+
+    chat_name = Column(String(130))
 
     moderation_level = Column(Integer(), default=100)
 
