5cae2bdb357687cf1a29e02fa68d0ea0997e1b84
Badsnus <anton.098.bal@gmail.com>
2024-03-19 10:33:47 +0300

    add threads for creation logs

--- src/services/send_logs.py
+++ src/services/send_logs.py
@@ -1,9 +1,20 @@
+import asyncio
+import concurrent.futures
 import os
 import time
+from typing import Sequence
 
 from aiogram.types import CallbackQuery, FSInputFile
 
+from src.models import Log
 from src.repo import DB
+
+
+def process_logs(logs: Sequence[Log]) -> str:
+    logs_text = ''
+    for log in logs:
+        logs_text += f'{log.chat_name} | {log.user_name} | {log.message} | {log.time}\n'
+    return logs_text
 
 
 async def send_logs(call: CallbackQuery, db: DB, id: int) -> None:
@@ -14,9 +25,12 @@ async def send_logs(call: CallbackQuery, db: DB, id: int) -> None:
 
     logs = await db.log.get_logs(call.from_user.id, id)
 
-    logs_text = ''
-    for log in logs:
-        logs_text += f'{log.chat_name} | {log.user_name} | {log.message} | {log.time}\n'
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        logs_text = await asyncio.get_event_loop().run_in_executor(
+            executor,
+            process_logs,
+            logs,
+        )
 
     if logs_text == '':
         await call.answer('Логов нет')
