523ec646fb15de34edb27033ef24436a852112e7
Badsnus <anton.098.bal@gmail.com>
2024-03-19 09:56:16 +0300

    delete useless info log update text

--- src/handlers/delete_useless_info.py
+++ src/handlers/delete_useless_info.py
@@ -9,17 +9,35 @@ router: Router = Router()
 
 
 async def create_chat_member_log(message: Message, db: DB, join: bool, bot: Bot) -> None:
+    if join:
+        # aiogram lmao??? TODO написать исью
+        user_id = message.new_chat_member.get('id')
+        user_name = str(message.new_chat_member.get('username'))
+
+        if user_id == message.from_user.id:
+            text = 'user join'
+        else:
+            text = f'user added by {message.from_user.username}'
+    else:
+        user_id = message.left_chat_member.id
+        user_name = str(message.left_chat_member.username)
+
+        if user_id == message.from_user.id:
+            text = 'user leave'
+        else:
+            text = f'user kicked by {message.from_user.username}'
+
     try:
         await message.delete()
         await update_chat_info(message, db, bot)
 
         await db.log.create(
             chat_id=message.chat.id,
-            user_id=message.from_user.id,
-            message='user join' if join else 'user leave',
+            user_id=user_id,
+            message=text,
             time=message.date,
             chat_name=message.chat.title,
-            user_name=str(message.from_user.username),
+            user_name=user_name,
         )
     except TelegramBadRequest as ex:
         # todo добавить удаление канала, если юзер там не админ мб? типа если он есть в бд - удалить из бд его
