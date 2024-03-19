efde2ebadcf4f90b7b5f37ffbe90f027c34cfa5f
Badsnus <anton.098.bal@gmail.com>
2024-03-19 10:08:37 +0300

    delete pic with bad text and delete message with via bot

--- src/services/delete_bad_message.py
+++ src/services/delete_bad_message.py
@@ -9,25 +9,39 @@ from src.repo import DB
 bad_words = set(open('bad_words.txt', encoding='utf-8').read().splitlines())
 
 
-async def is_bad_text(text):
+def is_bad_text(text: str) -> bool:
     words = set(re.findall(r'\w+', text))
     return bool(words & bad_words)
 
 
-async def delete_bad_message(message: Message, db: DB, chat: Chat) -> None:
-    is_bad = await is_bad_text(message.text)
-    if is_bad and chat.moderation_level != 0:
-        try:
-            await message.delete()
+def is_from_bot(message: Message) -> bool:
+    return bool(message.via_bot)
 
-            await db.log.create(
-                chat_id=message.chat.id,
-                user_id=message.from_user.id,
-                message=message.text,
-                time=message.date,
-                chat_name=message.chat.title,
-                user_name=message.from_user.username,
-            )
-        except:
-            # todo ошибку надо обработать
-            pass
+
+async def delete_message_and_create_log(db: DB, message: Message, text: str) -> None:
+    try:
+        await message.delete()
+
+        await db.log.create(
+            chat_id=message.chat.id,
+            user_id=message.from_user.id,
+            message=text,
+            time=message.date,
+            chat_name=message.chat.title,
+            user_name=message.from_user.username,
+        )
+    except:
+        # todo ошибку надо обработать
+        pass
+
+
+async def delete_bad_message(message: Message, db: DB, chat: Chat) -> None:
+    text = message.text or message.caption or ''
+
+    is_bad = is_bad_text(text)
+
+    if is_bad and chat.moderation_level != 0:
+        await delete_message_and_create_log(db, message, text)
+
+    if is_from_bot(message):
+        await delete_message_and_create_log(db, message, 'создано ботом')
