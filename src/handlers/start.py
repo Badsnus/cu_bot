63589756b016b19422a55c177c94a282bf914d0d
Badsnus <anton.098.bal@gmail.com>
2024-03-20 13:22:27 +0300

    start handler without db now

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -2,17 +2,15 @@ from aiogram import F, Router
 from aiogram.types import Message
 
 from src.keyboards import main_menu
-from src.repo import DB
 from src.services import get_or_create_user
 
 router: Router = Router()
 
 
 @router.message((F.text == '/start') & (F.chat.type == 'private'))
-async def start_handler(message: Message, db: DB) -> None:
+async def start_handler(message: Message) -> None:
     await get_or_create_user(
         user_id=message.from_user.id,
-        db=db,
     )
 
     await message.answer(
