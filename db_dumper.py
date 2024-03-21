d72b74828c7c4b030987062e64ab7e2da106c00d
Badsnus <anton.098.bal@gmail.com>
2024-03-21 11:42:59 +0300

    fix linter in db_dumper

--- db_dumper.py
+++ db_dumper.py
@@ -8,7 +8,7 @@ load_dotenv()
 bot_token = getenv('BOT_DUMBER_TOKEN')
 chat_id = getenv('DB_DUMB_CHANNEL_ID')
 
-url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+url = f'https://api.telegram.org/bot{bot_token}/sendDocument'
 
 files = {'document': open('main.db', 'rb')}
 data = {'chat_id': chat_id}
@@ -16,4 +16,4 @@ data = {'chat_id': chat_id}
 response = post(url, files=files, data=data)
 
 if response.status_code != 200:
-    print("FATAL FATAL FATAL", response.text)  # mb need catch? and do anything
+    print('FATAL FATAL FATAL', response.text)  # mb need catch? and do anything
