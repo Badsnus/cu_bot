1e99d553d7ab152970c076bafc2c8ee2b3df562e
Anton Balov <badsnus@MacBook-Air-Anton.local>
2024-06-22 14:35:26 +0300

    fix linter

--- src/services/check_is_message_good.py
+++ src/services/check_is_message_good.py
@@ -6,7 +6,11 @@ from src.models import Chat, ChatModerationLevelEnum
 class MessageChecker:
     bad_words: set = set()
 
-    def __init__(self, chat: Chat, text: str, is_from_bot: bool, is_from_channel: bool) -> None:
+    def __init__(self,
+                 chat: Chat,
+                 text: str,
+                 is_from_bot: bool,
+                 is_from_channel: bool) -> None:
         self._chat = chat
         self._text = text.lower()
         self._is_from_bot = is_from_bot
@@ -20,4 +24,6 @@ class MessageChecker:
         if self._chat.moderation_level == ChatModerationLevelEnum.off.value:
             return True
 
-        return not (self._is_bad_text() or self._is_from_bot or self._is_from_channel)
+        return not (self._is_bad_text() or
+                    self._is_from_bot or
+                    self._is_from_channel)
